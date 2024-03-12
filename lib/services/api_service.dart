1ee2f856d77931f58d028d0d92b454ba371ef2da
Esteban Barragan <esteban@akaunt.io>
2024-03-11 23:21:55 -0700

    feat: change url

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,20 +3,20 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String url = "https://inboxmessages.herokuapp.com/message";
+  static const String url = "portfolio-web-server.onrender.com";
   static Future<String> sendMessage(
       String email, String message, String name) async {
-    http.Response response = await http.post(Uri.parse(url),
-        body: jsonEncode({
-          "email": email,
-          "message": message,
-          "name": name,
-          "app": "portfolio",
-        }),
-        headers: {
+    http.Response response =
+        await http.Client().post((Uri.https(url, 'message')),
+            body: jsonEncode({
+              "email": email,
+              "message": message,
+              "name": name,
+            }),
+            headers: {
           'content-type': "application/json",
-          "Accept": "application/json",
-          "Access-Control_Allow_Origin": "*"
+          "Access-Control_Allow_Origin": "*",
+          'Accept': '*/*'
         });
     return response.body;
   }
